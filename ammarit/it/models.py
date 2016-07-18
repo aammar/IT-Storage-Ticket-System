fbf2b81230a92975fcad389df84718423059b1d3
Hasan Al-Jawaheri <hasan.aljawaheri@gmail.com>
2016-07-18 15:57:48 +0300

    Fixed database

--- ammarit/it/models.py
+++ ammarit/it/models.py
@@ -1,19 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Owner(models.Model):
-	FName = models.CharField(max_length=200)
-	LName = models.CharField(max_length=200)
+  user = models.ForeignKey(User, unique=True)
+  FName = models.CharField(max_length=200)
+  LName = models.CharField(max_length=200)
   ownerType = models.CharField(max_length=200)
-	def __unicode__(self):
-		return unicode(FName + " " + LName)
+  def __unicode__(self):
+    return unicode(FName + " " + LName)
 
 class Item(models.Model):
-	Item_Number = models.IntegerField(primary_key=True)
-	Category = models.CharField(max_length=200)
-	Make = models.CharField(max_length=200)
-	Model = models.CharField(max_length=200)
-	def __unicode__(self):
-		return unicode(str(self.Item_Number))
+  Item_Number = models.IntegerField(primary_key=True)
+  Category = models.CharField(max_length=200)
+  Make = models.CharField(max_length=200)
+  Model = models.CharField(max_length=200)
+  def __unicode__(self):
+    return unicode(str(self.Item_Number))
 
