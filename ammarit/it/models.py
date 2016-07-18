d3db2449e78ae67d9dcfd2fac7f8e9e584a76cfa
Ammar <aabourah@qatar.cmu.edu>
2016-07-18 15:45:01 +0300

    added models

--- ammarit/it/models.py
+++ ammarit/it/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+class Employee(models.Model):
+	FName = models.CharField(max_length=200)
+	LName = models.CharField(max_length=200)
+	ID = models.IntegerField(primary_key=True)
+	def __unicode__(self):
+		return unicode(self.ID)
+
+class Storage(models.Model):
+	Item_Number = models.IntegerField(primary_key=True)
+	Quantity = models.IntegerField()
+	Category = models.CharField(max_length=200)
+	Make = models.CharField(max_length=200)
+	Model = models.CharField(max_length=200)
+	def __unicode__(self):
+		return unicode(self.Item_Number)
+
+class Room():
+
+
+
+
+
+class Item_List(models.Model):
+	ID_Tag = models.IntegerField(primary_key=True)
+	Item_Number = models.ForeignKey('Storage', on_delete=models.CASCADE)
+	def __unicode__(self):
+		return unicode(self.ID_Tag)
+
+
+class Employee_Item(models.Model):
+	ID = models.ForeignKey('Employee', on_delete=models.CASCADE)
+	ID_Tag = models.ForeignKey('Item_List', on_delete=models.CASCADE)
